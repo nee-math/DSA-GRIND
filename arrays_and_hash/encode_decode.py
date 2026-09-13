c08481354352b1592d0fd2318ceb19a4bc2ebf44
akanola <folarin@getolyves.com>
2026-09-13 21:58:06 +0100

    Implement encode/decode

--- arrays_and_hash/encode_decode.py
+++ arrays_and_hash/encode_decode.py
@@ -1,9 +1,26 @@
 def encode(strs):
-    pass
+    result = ""
+    for s in strs:
+        size = len(s)
+        result += f"{size}#{s}"
+    return result
 
 
 def decode(s):
-    pass
+    result = []
+    length = len(s)
+    i = 0
+    while i < length:
+        j = i
+        while s[j] != "#":
+            j += 1
+
+        size = int(s[i: j])
+        j += 1
+        word = s[j: j + size]
+        result.append(word)
+        i = j + size
+    return result
 
 
 if __name__ == '__main__':
