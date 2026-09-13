e60355466b42f410f52229b4ebacb8673d3482f7
akanola <folarin@getolyves.com>
2026-09-13 21:11:17 +0100

    Implement top_k

--- arrays_and_hash/top_k.py
+++ arrays_and_hash/top_k.py
@@ -1,6 +1,27 @@
-def top_k(nums, k):
-    pass
+import math
+from collections import Counter
 
+
+def top_k(nums, k):
+    nums_freq = Counter(nums)
+    top_n = nums_freq.most_common(k)
+
+    return [item[0] for item in top_n]
+
+
+def get_highest_value(s):
+    ans = ()
+    max_count = 0
+
+    for k, v in s.items():
+        if v > max_count:
+            ans = (k, v)
+            max_count = v
+    return ans
+
+
+# Space O(n * m)
+# Time O(n)
 
 if __name__ == '__main__':
     nums = [1, 1, 1, 2, 2, 3]
